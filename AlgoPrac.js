31d1b9ec857847d8d681fcf80b2e0482a5aaed57
Bob Brown <robertbernardbrown@gmail.com>
2018-01-05 13:43:53 -0500

    adds bubbleSort to sheet

--- AlgoPrac.js
+++ AlgoPrac.js
@@ -1,4 +1,4 @@
-var arrayUse = [-33, 900, 5, 22, 20, -5, 17, 55, 52, 5, 6];
+var arrayUse = [-33, 900, 5, 22, 20, -5, 17, 55, 52, 5];
 
 //=====================================================================================================================
 //SELECTION SORT
@@ -14,47 +14,95 @@ var arrayUse = [-33, 900, 5, 22, 20, -5, 17, 55, 52, 5, 6];
 //BUBBLESORT
 //=====================================================================================================================
 
-function bubbleSort (array) {
-
-    for (var i in array) {
-
-        if (array[i] > array[i + 1]) {
-            array.splice(i, 1, i + 1);
-            array.splice(i + 1, 1, i);
-        } else {
-            return
+function bubbleSort(array) {
+    var isSorted = false;
+    
+    while (!isSorted) {
+      isSorted = true;
+      for (var index = 1; index < array.length; index++) {
+        
+        if (array[index - 1] > array[index]) {
+          isSorted = false;
+          
+          var temporaryReference = array[index - 1];
+          array[index - 1] = array[index];
+          array[index] = temporaryReference;
         }
+      }
     }
+    return array;
+  }
 
-}
+console.log(bubbleSort(arrayUse));
+
+//NOTES
+
+//Time Complexity: Quadratic O(n^2)
+//Space Complexity: in-place
+//Stability: Stable
+//Internal/External: Internal
+//Recursive?: No
+//Comparison?: Yes
+
+
+//WITH EXPLANATION FROM BASECS
+
+// function bubbleSort(array) {
+//     var isSorted = false;
+    
+//     while (!isSorted) {
+//       isSorted = true;
+//       // Iterate until we get to the last element
+//       for (var index = 1; index < array.length; index++) {
+//         console.log("comparing " + array[index] + " and " + array[index - 1]);
+        
+//         // If the element to the left is bigger, then swap the element
+//         // that we're currently looking at with its left neighbor.
+//         if (array[index - 1] > array[index]) {
+//           isSorted = false;
+//           console.log("SWAPPING " + array[index] + " and " + array[index - 1]);
+          
+//           // Swap elements by creating a temporary reference.
+//           var temporaryReference = array[index - 1];
+//           array[index - 1] = array[index];
+//           array[index] = temporaryReference;
+//         }
+//         console.log('array is now ', array);
+//       }
+//       console.log("**one full pass through array**");
+//       console.log("***is array sorted? ", isSorted);
+//     }
+    
+//     return array;
+//   }
 
 //=====================================================================================================================
 //INSERTION SORT
 //=====================================================================================================================
 
 
-function insertionSort (array) {
+// function insertionSort (array) {
 
-        for (var i = 0; i < array.length; i++) {
+//         for (var i = 0; i < array.length; i++) {
             
-            var currentUnsortedItem = array[i];
-            console.log('currentUnsortedItem is currently ' + currentUnsortedItem);
+//             var currentUnsortedItem = array[i];
+//             console.log('currentUnsortedItem is currently ' + currentUnsortedItem);
 
-            for (var j = i; j > 0 && currentUnsortedItem < array[j - 1]; j--) {
-                console.log(currentUnsortedItem + ' < ' + array[j-1] + ' is ' + (currentUnsortedItem < array[j - 1]));
-                array[j] = array[j - 1];
-                console.log('** inserting ' + array[j - 1] + ' at index ' + j);
-              }
+//             for (var j = i; j > 0 && currentUnsortedItem < array[j - 1]; j--) {
+//                 console.log(currentUnsortedItem + ' < ' + array[j-1] + ' is ' + (currentUnsortedItem < array[j - 1]));
+//                 array[j] = array[j - 1];
+//                 console.log('** inserting ' + array[j - 1] + ' at index ' + j);
+//               }
 
-            array[j] = currentUnsortedItem;
-            console.log('** inserting ' + currentUnsortedItem + ' at index ' + j);
+//             array[j] = currentUnsortedItem;
+//             console.log('** inserting ' + currentUnsortedItem + ' at index ' + j);
 
-            console.log('array is now: ' + array);
-        }
-        return array;
-}
+//             console.log('array is now: ' + array);
+//         }
+//         return array;
+// }
 
-console.log(insertionSort(arrayUse));
+// console.log(insertionSort(arrayUse));
 
 //NOTES
 
