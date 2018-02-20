86a333dbadb216697a0101c90874230ef6548f57
Bob Brown <robertbernardbrown@gmail.com>
2018-02-19 20:22:39 -0500

    adds quicksort to list

--- AlgoPrac.js
+++ AlgoPrac.js
@@ -217,51 +217,51 @@ var arrayUse = [-33, 900, 5, 22, 20, -5, 17, 55, 52, 5];
 //=====================================================================================================================
 
 
-function mergeSort(array) {
-  var arraySize = array.length;
+// function mergeSort(array) {
+//   var arraySize = array.length;
 
-  if (arraySize === 1) {
-    return;
-  }
+//   if (arraySize === 1) {
+//     return;
+//   }
 
-  var midpoint = Math.floor(arraySize / 2);
-  var leftArray = array.slice(0, midpoint);
-  var rightArray = array.slice(midpoint);
+//   var midpoint = Math.floor(arraySize / 2);
+//   var leftArray = array.slice(0, midpoint);
+//   var rightArray = array.slice(midpoint);
 
-  mergeSort(leftArray);
-  mergeSort(rightArray);
+//   mergeSort(leftArray);
+//   mergeSort(rightArray);
 
-  merge(leftArray, rightArray, array);
+//   merge(leftArray, rightArray, array);
 
-  return array;
-}
+//   return array;
+// }
 
-function merge(leftArray, rightArray, array) {
-  var index = 0;
+// function merge(leftArray, rightArray, array) {
+//   var index = 0;
 
-  while (leftArray.length && rightArray.length) {
-    console.log('array is: ', array);
-    if (rightArray[0] < leftArray[0]) {
-      array[index++] = rightArray.shift();
-    } else {
-      array[index++] = leftArray.shift();
-    }
-  }
+//   while (leftArray.length && rightArray.length) {
+//     console.log('array is: ', array);
+//     if (rightArray[0] < leftArray[0]) {
+//       array[index++] = rightArray.shift();
+//     } else {
+//       array[index++] = leftArray.shift();
+//     }
+//   }
 
-  while (leftArray.length) {
-    console.log('left array is: ', leftArray);
-    array[index++] = leftArray.shift();
-  }
+//   while (leftArray.length) {
+//     console.log('left array is: ', leftArray);
+//     array[index++] = leftArray.shift();
+//   }
 
-  while (rightArray.length) {
-    console.log('right array is: ', rightArray);
-    array[index++] = rightArray.shift();
-  }
+//   while (rightArray.length) {
+//     console.log('right array is: ', rightArray);
+//     array[index++] = rightArray.shift();
+//   }
 
-  console.log('** end of merge function ** array is: ', array);
-}
+//   console.log('** end of merge function ** array is: ', array);
+// }
 
-mergeSort(arrayUse);
+// mergeSort(arrayUse);
 
 
 //NOTES
@@ -328,3 +328,110 @@ mergeSort(arrayUse);
 
 //     console.log('** end of merge function ** array is: ', array);
 //   }
+
+//=====================================================================================================================
+//QUICK SORT
+//=====================================================================================================================
+
+function quickSort(items, leftIndex, rightIndex) {
+    // Declare an index that will be our pivot reference.
+    var pivotIndex;
+  
+    // If the array has only one item, it's already sorted!
+    // If it has no items, we don't want to try to sort it!
+    if (items.length > 1) {
+      // As long as the array has two items, we can parition it.
+      pivotIndex = partition(items, leftIndex, rightIndex);
+  
+      console.log('** pivot is: ', items[pivotIndex]);
+  
+      // If the left reference hasn't been incremented to
+      // reach the pivot yet, we want to keep comparing it.
+      if (leftIndex < pivotIndex - 1) {
+        quickSort(items, leftIndex, pivotIndex - 1);
+      }
+  
+      // If the right reference hasn't reached the 
+      // pivotIndex yet, we need to keep comparing it.
+      if (pivotIndex < rightIndex) {
+        quickSort(items, pivotIndex, rightIndex);
+      }
+  
+    }
+  
+    return items;
+}
+
+  // The partition() method takes a list of items, and a left
+// reference, as well as a right reference. Both left and right
+// are indexes to indicate where the pointers should start.
+function partition(items, left, right) {
+    // Find the pivot by adding the two indexes together
+    // and dividing by two (the middle element, effectively).
+    var pivot = items[Math.floor((right + left) / 2)];
+    var l = left;
+    var r = right;
+  
+    console.log('** pivot is: ', pivot);
+    console.log('** left is: ', items[left]);
+    console.log('** right is: ', items[right]);
+  
+    // Once the left reference is greater than the right reference,
+    // we have finished sorting this set of items, and we can return.
+    while (l <= r) {
+      // If the left pointer is less than the pivot, increment it.
+      // In other words, move the pointer to the right.
+      while (items[l] < pivot) {
+        l++;
+        console.log('l is now pointing to: ', items[l]);
+      }
+  
+      // If the right pointer is greater than the pivot, decrement it.
+      // In other words, move the pointer to the left.
+      while (items[r] > pivot) {
+        r--;
+        console.log('r is now pointing to: ', items[r]);
+      }
+  
+      // If the left pointer is larger than the pivot, and the right
+      // pointer is not bigger than the pivot, swap the two elements.
+      if (l <= r) {
+        console.log('** now swapping l and r pointers: ', items[l], items[r]);
+  
+        swap(items, l, r);
+  
+        // After swapping, increment/decrement the pointers respectively.
+        l++;
+        r--;
+  
+        // console.log('l is now pointing to: ', items[l]);
+        // console.log('r is now pointing to: ', items[r]);
+      }
+}
+  
+    // The left item becomes the new pivot element.
+    return l;
+  }
+
+function swap(items, leftPointerIndex, rightPointerIndex){
+    // Create a temporary reference for the left item.
+    var tempReference = items[leftPointerIndex];
+
+    // Move left item to the index that contains right item.
+    // Move right item to the temporary reference.
+    items[leftPointerIndex] = items[rightPointerIndex];
+    items[rightPointerIndex] = tempReference;
+}
+
+console.log(arrayUse);
+quickSort(arrayUse, 0, arrayUse.length - 1);
+console.log(arrayUse);
+
+//NOTES
+
+//Time Complexity: O (n log n)
+//Space Complexity: in place (though technically needs a little space for pointers)
+//Stability: unstable
+//Internal/External: internal
+//Recursive?: Y
+//Comparison?: Y
