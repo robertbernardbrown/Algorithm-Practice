1a6167bf638896c71fad40e748a3496a29fad6b2
Bob Brown <robertbernardbrown@gmail.com>
2018-06-06 18:59:28 -0400

    adds binheap algo practice

--- AlgoPrac.py
+++ AlgoPrac.py
@@ -299,7 +299,56 @@ class binHeap2:
 		return self.heapSize
 
 
-bh = binHeap2()
+
+class binHeap3:
+	def __init__(self):
+		self.heap = [0]
+		self.heapSize = 0
+	def percUp(self, i):
+		while i // 2 > 0:
+			if self.heap[i] < self.heap[i//2]:
+				temp = self.heap[i]
+				self.heap[i] = self.heap[i//2]
+				self.heap[i//2] = temp
+			i = i//2
+	def insert(self, k):
+		self.heap.append(k)
+		self.heapSize = self.heapSize + 1
+		self.percUp(self.heapSize)
+	def percDown(self, i):
+		while i * 2 > 0:
+			mc = self.minChild(i)
+			if self.heap[i] > self.heap[mc]:
+				temp = self.heap[i]
+				self.heap[i] = self.heap[mc]
+				self.heap[mc] = temp
+			i = mc
+	def minChild(self, i):
+		if i * 2 + 1 < self.heapSize:
+			return i * 2
+		else:
+			if self.heap[i*2] < self.heap[i*2+1]:
+				return self.heap[i*2]
+			else:
+				return self.heap[i*2+1]
+	def delMin(self):
+		retVal = self.heap[1]
+		self.heap[1] = self.heap[self.heapSize]
+		self.heapSize = self.heapSize - 1
+		self.heap.pop()
+		self.percDown(1)
+		return retVal
+	def buildHeap(self, alist):
+		i = len(alist) // 2
+		self.heapSize = len(alist)
+		self.heap = [0] + alist[:]
+		while i > 0:
+			self.percDown(i)
+			i = i - 1
+		return self.heapSize
+
+
+bh = binHeap3()
 bh.insert(2)
 bh.insert(3)
 bh.insert(80)
