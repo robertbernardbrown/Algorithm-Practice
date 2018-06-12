fa5dddca72a4e6220a5bb9f916d58f0e68d706dc
Bob Brown <robertbernardbrown@gmail.com>
2018-06-12 14:48:20 -0400

    adds hashtable practice

--- AlgoPrac.py
+++ AlgoPrac.py
@@ -553,33 +553,25 @@ class HashMap:
 
 class HashMap2:
 	def __init__(self):
-		self.size = 6
-		self.map = [None] * self.size
+		self.mapSize = 6
+		self.map = self.mapSize * [None]
 	def _get_hash(self, key):
 		hash = 0
 		for char in str(key):
 			hash += ord(char)
-		return hash % self.size
+		return hash % self.mapSize
 	def add(self, key, value):
 		key_hash = self._get_hash(key)
 		key_value = [key, value]
 		if self.map[key_hash] is None:
 			self.map[key_hash] = list([key_value])
-			return True
 		else:
 			for pair in self.map[key_hash]:
 				if pair[0] == key:
 					pair[1] = value
 					return True
-			self.map[key_hash].append(key)
+			self.map[key_hash].append(key_value)
 			return True
-	def get(self, key):
-		key_hash = self._get_hash(key)
-		if self.map[key_hash] is not None:
-			for pair in self.map[key_hash]:
-				if pair[0] == key:
-					return pair[1]
-		return None
 	def delete(self, key):
 		key_hash = self._get_hash(key)
 		if self.map[key_hash] is None:
@@ -589,11 +581,17 @@ class HashMap2:
 				self.map[key_hash].pop(i)
 				return True
 		return False
-	def printIt(self):
-		for key in self.map:
-			if key is not None:
-				print(str(key))
-			
+	def get(self, key):
+		key_hash = self._get_hash(key)
+		if self.map[key_hash] is not None:
+			for pair in self.map[key_hash]:
+				if pair[0] == key:
+					return pair[1]
+		return None
+	def printHash(self):
+		for item in self.map:
+			if item is not None:
+				print(str(item))
 
 h = HashMap2()
 h.add('Bob', '567-8888')
@@ -604,10 +602,10 @@ h.add('Aditya', '852-6551')
 h.add('Alicia', '632-4123')
 h.add('Mike', '567-2188')
 h.add('Aditya', '777-8888')
-h.printIt()		
+h.printHash()		
 h.delete('Bob')
-h.printIt()
-print('Ming: ' + h.get('Ming'))
+h.printHash()
+print('Ming: ', h.get('Ming'))
 print(h.get('Ankit'))
 
 
