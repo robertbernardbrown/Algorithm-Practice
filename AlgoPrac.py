4a6f862c614ecddedcbe12b83325a57becb2db66
Bob Brown <robertbernardbrown@gmail.com>
2018-06-19 11:37:51 -0400

    practice quicksort

--- AlgoPrac.py
+++ AlgoPrac.py
@@ -693,44 +693,63 @@ print(alist)
 
 def quickSort(alist):
 	quickSortHelper(alist,0,len(alist)-1)
-
 def quickSortHelper(alist,first,last):
 	print("quicksort", alist, first, last)
 	if first<last:
 		splitpoint = partition(alist,first,last)
 		quickSortHelper(alist,first,splitpoint-1)
 		quickSortHelper(alist,splitpoint+1,last)
-
-
 def partition(alist,first,last):
    print("partition", alist, first, last)
    pivotvalue = alist[first]
-
    leftmark = first+1
    rightmark = last
-
    done = False
    while not done:
-
        while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
            leftmark = leftmark + 1
-
        while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
            rightmark = rightmark -1
-
        if rightmark < leftmark:
            done = True
        else:
            temp = alist[leftmark]
            alist[leftmark] = alist[rightmark]
            alist[rightmark] = temp
-
    temp = alist[first]
    alist[first] = alist[rightmark]
    alist[rightmark] = temp
-
-
    return rightmark
+
+
+
+def quickSort2(alist):
+	quickSortHelper2(alist, 0, len(alist)-1)
+def quickSortHelper2(alist, first, last):
+	if first < last:
+		split = partition2(alist, first, last)
+		quickSortHelper2(alist, first, split-1)
+		quickSortHelper2(alist, split + 1, last)
+def partition2(alist, first, last):
+	pivotValue = alist[first]
+	leftmark = first+1
+	rightmark = last
+	done = False
+	while not done:
+		while leftmark <= rightmark and alist[leftmark] <= pivotValue:
+			leftmark = leftmark + 1
+		while alist[rightmark] >= pivotValue and rightmark >=leftmark:
+			rightmark = rightmark - 1
+		if rightmark < leftmark:
+			done = True
+		else:
+			temp = alist[leftmark]
+			alist[leftmark] = alist[rightmark]
+			alist[rightmark] = temp
+	temp = alist[first]
+	alist[first] = alist[rightmark]
+	alist[rightmark] = temp
+	return rightmark
 
 alist = [54,26,93,17,77,31,44,55,20]
 # quickSort(alist)
